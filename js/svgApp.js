44c8145cb60e2e4f902b667eafcca266df875185
Caitlin Potter <caitpotter88@gmail.com>
2014-09-09 21:03:37 -0400

    Update 2014-09-10T01:03:37.449Z

--- js/svgApp.js
+++ js/svgApp.js
@@ -82,4 +82,73 @@ angular.module("svgApp", []).
             'Arc-To',
             'Close-Path'
         ];
+    }).controller("artCtrl", function($scope, $element, $interval) {
+      var self = this;
+      this.strokeWidth = 1;
+      this.fillColour = "#000";
+      this.strokeColour = "#888";
+      this.nodes = 1;
+      this.points = 2;
+      var cachedNodes = [];
+
+      function getSvgRoot() {
+        return $element[0].querySelector('svg');
+      }
+
+      this.makeNodes = function(nodes) {
+        cachedNodes.length = nodes;
+        return cachedNodes;
+      }
+
+      this.radius = 100;
+
+      function moveTo(x, y) {
+        if (x !== x || y !== y) return '';
+        return 'M ' + x + ',' + y;
+      }
+
+      function lineTo(x, y) {
+        if (x !== x || y !== y) return '';
+        return 'L ' + x + ',' + y;
+      }
+
+      var DEGREES_TO_RADIANS = Math.PI / 180;
+      var TO_RADIANS = function(degrees) { return degrees * DEGREES_TO_RADIANS; }
+      var counter = 0;
+
+      $interval(function() {
+        counter += .25;
+      }, 1000 / 60);
+
+      this.makePath = function(index, count) {
+        var svg = getSvgRoot();
+        var w = svg.offsetWidth;
+        var h = svg.offsetHeight;
+        var mx = w / 2;
+        var my = h / 2;
+        var deg = TO_RADIANS((360 / count) * (index+1) + counter);
+        var rx = mx + (self.radius * Math.sin(deg));
+        var ry = my + (self.radius * Math.cos(deg));
+        deg += TO_RADIANS(60);
+        var rx2 = rx + (25 * Math.sin(deg));
+        var ry2 = ry + (25 * Math.cos(deg));
+        deg -= TO_RADIANS(120);
+        var rx3 = rx + (25 * Math.sin(deg));
+        var ry3 = ry + (25 * Math.cos(deg));
+        return [moveTo(mx, my), lineTo(rx, ry),
+                moveTo(rx2, ry2), lineTo(rx, ry),
+                moveTo(rx3, ry3), lineTo(rx, ry)].join(' ');
+      }
+
+      function mixColour(colour, index) {
+        return colour;
+      }
+
+      this.mixFill = function(index) {
+        return mixColour(self.fillColour, index);        
+      }
+
+      this.mixStroke = function(index) {
+        return mixColour(self.strokeColour, index);
+      }
     });
